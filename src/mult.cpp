c6f17570a4f0fa7c5818748d6c5f4c88ff2a68d0
gustavohbo <ghboliveira@hotmail.com>
2018-12-09 01:14:07 -0300

    Code working

--- src/mult.cpp
+++ src/mult.cpp
@@ -1,49 +1,49 @@
-#include <sys/types.h>  /* for type definitions */
-#include <sys/socket.h> /* for socket API function calls */
-#include <netinet/in.h> /* for address structs */
-#include <arpa/inet.h>  /* for sockaddr_in */
-#include <stdio.h>      /* for printf() */
-#include <stdlib.h>     /* for atoi() */
-#include <string.h>     /* for strlen() */
-#include <unistd.h>     /* for close() */
-#include <iostream>
-
-class Multicast
-{
-  private:
-    /* data */
-    const int MAX_LEN = 1024;
-    const int MIN_PORT = 1024;
-    const int MAX_PORT = 65535;
-    const int MAX_BUFFER_SIZE = 1024; 
-    char *ip_group;
-    int port;
-    struct sockaddr_in address;
-    int socketNumber;
-    char *data;
-
-  public:
-    Multicast(char *ip, int port);
-    ~Multicast();
-    int sendSocketMessage(char *message);
-};
+#include "./include/Multicast.hpp"
 
 Multicast::Multicast(char *ip, int port)
 {
-    ip_group = ip;
-    this->port = port;
-    /* create what looks like an ordinary UDP socket */
-    if ((socketNumber = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    if (port < MIN_PORT || port > MAX_PORT)
     {
-        perror("socket");
-        exit(1);
+        perror("Port invalid!");
     }
+    else
+    {
+        ip_group = ip;
+        this->port = port;
 
-    /* set up destination address */
-    memset(&address, 0, sizeof(address));
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = inet_addr(ip_group);
-    address.sin_port = htons(port);
+        /* create what looks like an ordinary UDP socket */
+        if ((socketNumber = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+        {
+            perror("socket");
+            exit(1);
+        }
+
+        /* set up destination address */
+        memset(&address, 0, sizeof(address));
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = inet_addr(ip_group);
+        address.sin_port = htons(port);
+        u_int len = 1;
+        if (setsockopt(socketNumber, SOL_SOCKET, SO_REUSEADDR, &len, sizeof(len)) < 0)
+        {
+            perror("Reusing ADDR failed");
+            exit(1);
+        }
+        /* bind to receive address */
+        if (bind(socketNumber, (struct sockaddr *)&address, sizeof(address)) < 0)
+        {
+            perror("bind");
+            exit(1);
+        }
+        /* use setsockopt() to request that the kernel join a multicast group */
+        mreq.imr_multiaddr.s_addr = inet_addr(ip_group);
+        mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+        if (setsockopt(socketNumber, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0)
+        {
+            perror("setsockopt");
+            exit(1);
+        }
+    }
 }
 
 Multicast::~Multicast()
@@ -51,7 +51,7 @@ Multicast::~Multicast()
     close(socketNumber);
 }
 
-int Multicast::sendSocketMessage(char *msg)
+int Multicast::sendMessage(char *msg)
 {
     if (sendto(socketNumber, msg, sizeof(msg), 0, (struct sockaddr *)&address, sizeof(address)) < 0)
     {
@@ -60,9 +60,16 @@ int Multicast::sendSocketMessage(char *msg)
     }
 }
 
-int main()
+char *Multicast::receiveMessage()
 {
-    Multicast m("224.0.0.1", 65534);
-    m.sendSocketMessage("oi");
-    return 0;
+    addrlen = sizeof(address);
+    if ((nbytes = recvfrom(socketNumber, buffer_data, MAX_BUFFER_SIZE, 0, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+    {
+        perror("recvfrom");
+        //exit(1);
+    }
+    if (nbytes > 0)
+    {
+        return buffer_data;
+    }
 }
